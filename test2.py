ac230e238ce0fc38ea3a896d7b35491afb8c052a
miksuk28 <74315076+miksuk28@users.noreply.github.com>
2021-01-22 13:26:51 +0100

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,23 @@
+import pigpio
+import time
+pi = pigpio.pi()
+
+RED = 17
+GREEN = 22
+BLUE = 24
+
+def led_off():
+    led_set(0,0,0)
+    print("Strip slått av")
+
+def led_set(r, g, b):
+    pi.set_PWM_dutycycle(RED, r)
+    pi.set_PWM_dutycycle(GREEN, g)
+    pi.set_PWM_dutycycle(BLUE, b)
+    print("Satt LED verdi: (" + str(r) + ", " + str(g) + ", " + str(b) + ")")
+
+led_off()
+for i in range(255):
+    led_set(i, i, i)
+    print(i)
+    time.sleep(0.1)
