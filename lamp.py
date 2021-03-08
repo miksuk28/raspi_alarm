a3c3a1a3c34978ef81445f958df303c30ac1fcda
miksuk28 <74315076+miksuk28@users.noreply.github.com>
2021-03-08 15:17:38 +0100

    la til kommentarar

--- lamp.py
+++ lamp.py
@@ -1,17 +1,21 @@
+# debug modus for testing
 debug = False
 
-from time import *
-from datetime import *
-from threading import Thread
-import configparser
+from time import *              # sleep() for å kunne vente
+from datetime import *          # handtering av dagar og klokkeslett
+from threading import Thread    # threading, for å kunne ha ein timar som køyrer parallellt i bakgrunnen
+import configparser             # for å lese config filer der alarmane og innstillingane er lagra
 
+# importerer ikkje Raspi moduler dersom debug == True
 if not debug:
     import pigpio
     from subprocess import call
 
+    # caller pigpiod gjennom terminalen
     call("sudo pigpiod", shell=True)
     pi = pigpio.pi()
 
+# pinnummer for lys og brytar
 R = 17
 G = 22
 B = 24
@@ -19,17 +23,18 @@ BTN = 27
 
 weekdays = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
 
+# laster inn og les konfigurasjonsfila
 config = configparser.ConfigParser()
 config.read("config.ini")
-running = True
 
-# testing
+# sjekker om knapp er på når kalla: returnerer bool (om pin 27 ikkje les 0)
 def check_button():
     if pi.read(BTN) != 0:
         return True
     else:
         return False
 
+# simpelt klokkeobjekt: ikkje så veldig nyttig, bør kanskje fjernast
 class Clock:
     def __init__(self):
         self.h = int(datetime.now().strftime("%H"))
@@ -38,7 +43,7 @@ class Clock:
 
 clock = Clock()
 
-# koyr i anna thread
+# timer som køyrer i anna thread uavhengig av hovudprogrammet
 def timer():
     global time_mins
     while True:
@@ -48,22 +53,26 @@ def timer():
         print(time_mins)
         sleep(1)
 
+# deklarerer og starter threaden
 timer_thread = Thread(target = timer)
 timer_thread.start()
 
+# omrekner og returnerer timar og minutt til tal minutt
 def calc_mins(hours, minutes):
     return (hours * 60) + minutes
 
+# oppdaterer alarmen: les frå konfig-fila på nytt og lagrar framtidige alarmar i ei liste
 def update_alarm():
     config.read("config.ini")
 
-    # sletter alt innholder i lista
+    # sletter alt innholder i listene
     global alarms
     global settings
     alarms = []
     settings = []
     day = datetime.now().weekday()
-
+    
+    # setter preferansane
     settings = [
         {"lamp_mode" : {"red" : int(config["LAMP_MODE"]["RED"]), "green" : int(config["LAMP_MODE"]["GREEN"]), "blue" : int(config["LAMP_MODE"]["BLUE"])}},
         {"pref" : {"red" : int(config["PREF"]["RED"]), "green" : int(config["PREF"]["GREEN"]), "blue" : int(config["PREF"]["BLUE"]), "offset" : int(config["PREF"]["offset"])}}
@@ -71,13 +80,14 @@ def update_alarm():
     # alarms = [{"day" : "Monday", "alarm_state" : True, "alarm_hour" : 6, "alarm_minute" : 10}]
     
     # sjekker berre tom. sondag. Legg til unntak for sondag-mandag
+    # cycler gjennom og legger til framtidige alarmer til lista, sjekker i config fila
     for i in range(day, 7):
         if config[weekdays[i]]["alarm_state"] == "1":
             mins = calc_mins(int(config[weekdays[i]]["alarm_hour"]), int(config[weekdays[i]]["alarm_minute"]))
             alarm_day = {"day" : weekdays[i], "alarm_state" : True, "alarm_hour" : int(config[weekdays[i]]["alarm_hour"]), "alarm_minute" : int(config[weekdays[i]]["alarm_minute"]), "mins" : mins, "mins_offset" : mins - settings[1]["pref"]["offset"]}
             alarms.append(alarm_day)
 
-
+# setter led verdi og lagrer noverande verdi i globale variablar
 def led_set(r, g, b):
     global red, green, blue
     red = r
@@ -90,9 +100,11 @@ def led_set(r, g, b):
 
     if debug: print(r, g, b)
 
+# slår av leds
 def led_off():
     led_set(0,0,0)
 
+# linær interpolasjon for fading mellom to verdiar, funkar sikkert ikkje enda
 def interpolate(r1, g1, b1, r2, g2, b2, steps, pause):
     r_ = (r2 - r1) / steps
     g_ = (g2 - g1) / steps
@@ -105,6 +117,7 @@ def interpolate(r1, g1, b1, r2, g2, b2, steps, pause):
 def fade_off():
     pass
 
+# potensiell boot-animasjon
 def boot_animation():
     led_off()
     p = 0.005 # pause
@@ -125,6 +138,7 @@ print("gaming")
 
 print(settings[0]["lamp_mode"]["red"], settings[0]["lamp_mode"]["green"], settings[0]["lamp_mode"]["blue"])
 
+# main loop der alt skjer, bør kanskje vere i ein funksjon
 while True:
     led_set(settings[0]["lamp_mode"]["red"], settings[0]["lamp_mode"]["green"], settings[0]["lamp_mode"]["blue"])
     if check_button():
