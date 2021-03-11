1ad34d872ec69e501cae053c4f5e2bef5ca4e9f4
miksuk28 <74315076+miksuk28@users.noreply.github.com>
2021-03-11 08:51:26 +0100

    Update lamp.py

--- lamp.py
+++ lamp.py
@@ -144,7 +144,7 @@ def fade_off():
     b_ = (0 - blue) / steps
     
     r = red, g = green, b = blue
-    for i in range(60):
+    for i in range(0, steps):
         led_set(r - (r_ * i), g - (g_ * i), b - (b_ * i))
         sleep(0.01)
 
